857608252f5f7c272c52f8dd3e2c3fe3fae1765a
Diego J <71460884+DiegoJ2@users.noreply.github.com>
2023-06-26 19:27:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeImage() {
+    document.getElementById("lightbulb").src="https://cdn-icons-png.flaticon.com/512/1884/1884795.png";
+      
+    }
+    
+    function resetImage() {
+    document.getElementById("lightbulb").src="https://cdn-icons-png.flaticon.com/512/32/32299.png";
+      
+    }
+    
+    
